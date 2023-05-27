c2f133afb8260194d1ce633ca3930938402ff284
Lucas Simines <lucas@fmcx.digital>
2023-05-27 12:21:57 -0300

    build(frontend): vue 3 + typescript + laravel mix

---
+++ wp-content/themes/sage-mix-main/resources/scripts/App.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+const test = 'test';
+console.log(test);
+</script>
+
+<template>
+  <div>{{ test }}</div>
+</template>
--- wp-content/themes/sage-mix-main/webpack.mix.js
+++ wp-content/themes/sage-mix-main/webpack.mix.js
@@ -1,8 +1,31 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
+const path = require('path');
 
-mix.js('resources/scripts/app.js', 'dist').setPublicPath('public')
-.postCss("resources/styles/app.css", "public/css", [
-  require("tailwindcss"),
-]);
+const alias = {
+  '@': path.resolve(__dirname, 'resources/js'),
+};
+
+mix
+  .vue()
+  .alias(alias)
+  .js('resources/scripts/app.ts', 'dist')
+  .setPublicPath('public')
+  .postCss('resources/styles/app.css', 'public/css', [require('tailwindcss')]);
 
 mix.browserSync('http://localhost:10044');
+
+mix.webpackConfig({
+  resolve: {
+    extensions: ['.vue', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_module/,
+        options: {appendTsSuffixTo: [/\.vue$/]},
+      },
+    ],
+  },
+});
